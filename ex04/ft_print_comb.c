32d6d6c15d92402e109ce83f0475c6aadbab63f7
Thabo Mathebula <tmathebu@c4r18s1.wethinkcode.co.za>
2020-06-23 10:25:29 +0200

    still to make changes

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmathebu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 15:11:29 by tmathebu          #+#    #+#             */
+/*   Updated: 2020/06/19 15:36:15 by tmathebu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_print_comb(void);
+
+void ft_putchar(char a);
+
+int main (){
+
+ft_print_comb();
+
+return 0;
+
+}
+
+void ft_putchar(char a){
+	write(1,&a,1);
+}
+
+void ft_print_comb(void){
+	
+}
+	
+	ft_putchar('\n');
+}
